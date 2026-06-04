58ae1d9930e91a8e20fea289e769a6eb8ac281e0
Keerthana Saravanakumar <163813456+keerthanask15@users.noreply.github.com>
2026-06-04 09:18:06 +0530

    Obfuscate database password in DBConnection.java
    Replaced hardcoded database password with a placeholder.

--- src/util/DBConnection.java
+++ src/util/DBConnection.java
@@ -10,7 +10,7 @@ public class DBConnection {
 
     private static final String USER = "root";
 
-    private static final String PASSWORD = "Hazelshiny@15";
+    private static final String PASSWORD = "*********";
 
     public static Connection getConnection() {
 
